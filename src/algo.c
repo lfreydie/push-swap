25c961a72a2e16cad2c779ac24e5a077ed210391
Leonard FREYDIER <lfreydie@e2r3p13.clusters.42paris.fr>
2023-01-31 14:15:45 +0100

    retour maison

--- src/algo.c
+++ src/algo.c
@@ -3,14 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   algo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lefreydier <lefreydier@student.42.fr>      +#+  +:+       +#+        */
+/*   By: lfreydie <lfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 21:54:23 by lefreydier        #+#    #+#             */
-/*   Updated: 2023/01/31 01:07:59 by lefreydier       ###   ########.fr       */
+/*   Updated: 2023/01/31 13:45:39 by lfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+void	algo_pt(t_infos *piles)
+{
+	
+}
 
 void	algo_gd(t_infos *piles)
 {
@@ -19,5 +24,4 @@ void	algo_gd(t_infos *piles)
 	i = 0;
 	while (i++ < (piles->size / 2))
 		mv_pb(piles);
-	
 }
