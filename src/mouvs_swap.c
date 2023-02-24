8452a0a26b5b626fbdbbc0ef47cc984937138593
Leonard FREYDIER <lfreydie@e1r10p12.clusters.42paris.fr>
2023-02-24 12:36:27 +0100

    plus d'erreurs

--- src/mouvs_swap.c
+++ src/mouvs_swap.c
@@ -6,7 +6,7 @@
 /*   By: lfreydie <lfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 21:55:16 by lefreydier        #+#    #+#             */
-/*   Updated: 2023/02/23 18:38:29 by lfreydie         ###   ########.fr       */
+/*   Updated: 2023/02/24 12:22:11 by lfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,41 @@
 
 int	mv_sa(t_infos *piles)
 {
-	t_element	*tmp_a_f;
-	t_element	*tmp_a_s;
+	t_element	*tmp_a_1;
+	t_element	*tmp_a_2;
+	t_element	*tmp_a_3;
 
 	if (!piles->a || !piles->a->next)
 		return (0);
-	tmp_a_f = piles->a;
-	tmp_a_s = piles->a->next;
-	tmp_a_f->next = tmp_a_s->next;
-	tmp_a_f->prev = tmp_a_s;
-	tmp_a_s->next = tmp_a_f;
-	tmp_a_s->prev = NULL;
-	piles->a = tmp_a_s;
+	tmp_a_1 = piles->a;
+	tmp_a_2 = piles->a->next;
+	tmp_a_3 = tmp_a_2->next;
+	tmp_a_1->next = tmp_a_3;
+	tmp_a_1->prev = tmp_a_2;
+	tmp_a_2->next = tmp_a_1;
+	tmp_a_2->prev = NULL;
+	tmp_a_3->prev = tmp_a_1;
+	piles->a = tmp_a_2;
 	return (1);
 }
 
 int	mv_sb(t_infos *piles)
 {
-	t_element	*tmp_b_f;
-	t_element	*tmp_b_s;
+	t_element	*tmp_b_1;
+	t_element	*tmp_b_2;
+	t_element	*tmp_b_3;
 
 	if (!piles->b || !piles->b->next)
 		return (0);
-	tmp_b_f = piles->b;
-	tmp_b_s = piles->b->next;
-	tmp_b_f->next = tmp_b_s->next;
-	tmp_b_f->prev = tmp_b_s;
-	tmp_b_s->next = tmp_b_f;
-	tmp_b_s->prev = NULL;
-	piles->b = tmp_b_s;
+	tmp_b_1 = piles->b;
+	tmp_b_2 = piles->b->next;
+	tmp_b_3 = tmp_b_2->next;
+	tmp_b_1->next = tmp_b_2->next;
+	tmp_b_1->prev = tmp_b_2;
+	tmp_b_2->next = tmp_b_1;
+	tmp_b_2->prev = NULL;
+	tmp_b_3->prev = tmp_b_1;
+	piles->b = tmp_b_2;
 	return (1);
 }
 
